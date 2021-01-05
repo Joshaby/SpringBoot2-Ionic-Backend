6d96816e2dc613658d4f495a2915ca4d438dcb98
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2021-01-05 15:38:48 -0300

    Adicionado primeira classe de domínio!

---
+++ src/main/java/com/nelioalves/cursomc/domain/Categoria.java
@@ -0,0 +1,40 @@
+package com.nelioalves.cursomc.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Categoria implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private String nome;
+
+    public Categoria(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Categoria)) return false;
+        Categoria categoria = (Categoria) o;
+        return Objects.equals(id, categoria.id);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
