0088825da45960e5df4a8167fbe1a8c9c5511f65
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2021-01-04 21:56:16 -0300

    Adicionado projeto maven com Spring Boot

---
+++ README.md
@@ -0,0 +1,2 @@
+# Spring-Boot-Ionic
+Repositório de projetos do curso Spring Boot, Hibernate, REST, Ionic, JWT, S3, MySQL, MongoDB na Udemy por Nélio Alver
---
+++ src/test/java/com/nelioalves/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nelioalves.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
