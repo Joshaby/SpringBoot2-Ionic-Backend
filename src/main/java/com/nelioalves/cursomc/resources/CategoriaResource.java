a5850ce933e83567965a98057107853573d65721
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2021-01-04 22:30:00 -0300

    Adicionado controlador REST CategoriaResources mapeado ao endpoint /categorias e usando método GET do html

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -1,14 +1,15 @@
 package com.nelioalves.cursomc.resources;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/categorias")
+@RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
 	public String listar() {
-		return "REST está funcionando!";
+		return "Rest está funcionando!";
 	}
 }
