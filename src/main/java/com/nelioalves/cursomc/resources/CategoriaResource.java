d968b7b1b9d59963b2d07eaa641f2972ae65c1e0
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2021-01-05 17:31:06 -0300

    Adicionado controlador REST modificado!

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,24 @@
 package com.nelioalves.cursomc.resources;
 
+import com.nelioalves.cursomc.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
-@RequestMapping(value="/categorias")
-public class CategoriaResource {
-	
-	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando!";
-	}
+@RequestMapping(value = "/categorias")
+public class CategoriaResources {
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Categoria> listar() {
+        Categoria cat1 = new Categoria(1, "José");
+        Categoria cat2 = new Categoria(2, "Henrique");
+
+        List<Categoria> categoriasList = new ArrayList<>();
+        categoriasList.add(cat1);
+        categoriasList.add(cat2);
+
+        return categoriasList;
+    }
 }
