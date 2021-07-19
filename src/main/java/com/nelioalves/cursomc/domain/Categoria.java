389b97e9c411fc7eda112d3c7a8cd6dc02595a22
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2021-07-19 13:14:18 -0300

    Adicionado txt com comentários sobre as anotações do JPA!

--- src/main/java/com/nelioalves/cursomc/domain/Categoria.java
+++ src/main/java/com/nelioalves/cursomc/domain/Categoria.java
@@ -15,7 +15,7 @@ public class Categoria implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     @ManyToMany(mappedBy = "categoriasList")
