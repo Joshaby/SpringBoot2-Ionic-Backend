c58349c4c1fcb7b1866c8d7c33170daf5a21cc5d
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2021-08-06 11:06:05 -0300

    Adicionado configuração inicial do Spring Security!

---
+++ src/main/java/com/nelioalves/cursomc/config/SecurityConfig.java
@@ -0,0 +1,66 @@
+package com.nelioalves.cursomc.config;
+
+import java.util.Arrays;
+import org.springframework.http.HttpMethod;
+import org.springframework.core.env.Environment;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Classe de configuração de segurança
+ * @author José Henrique
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private Environment environment;
+
+    private static final String[] PUBLIC_MATCHERS = {
+            "/h2-console/**"
+    };
+
+    private static final String[] PUBLIC_MATCHERS_GET = {
+            "/produtos/**",
+            "/categorias/**"
+    };
+
+    /**
+     * Configura segurança de acesso a endpoints e estado da aplicação
+     * @param httpSecurity Objeto para configurar a segurança
+     * @throws Exception
+     */
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        if (Arrays.asList(environment.getActiveProfiles()).contains("test")) {
+            httpSecurity.headers().frameOptions().disable();
+        }
+        httpSecurity.cors().and().csrf().disable();
+        httpSecurity.authorizeRequests()
+                .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+                .antMatchers(PUBLIC_MATCHERS).permitAll()
+                .anyRequest().authenticated();
+        httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    /**
+     * Configuração para que a aplicação receba requisição de outra aplicação qualquer
+     * @return Um UrlBasesCorsConfigurationSource
+     */
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration().applyPermitDefaultValues();
+        final UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return urlBasedCorsConfigurationSource;
+    }
+}
