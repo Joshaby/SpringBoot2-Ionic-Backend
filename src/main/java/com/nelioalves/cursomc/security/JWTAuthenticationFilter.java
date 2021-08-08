ae5e172a5cb5514c7d68084f76882e4719d8c25b
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2021-08-07 21:18:17 -0300

    Adicionada classe JWTAuthenticationFailureHandler dentro da classe de autenticação para fazer com que a aplicação retorne o erro certo na hora de um login não efetuado!

--- src/main/java/com/nelioalves/cursomc/security/JWTAuthenticationFilter.java
+++ src/main/java/com/nelioalves/cursomc/security/JWTAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package com.nelioalves.cursomc.security;
 
+import java.util.Date;
 import java.io.IOException;
 import java.util.ArrayList;
 import javax.servlet.FilterChain;
@@ -12,6 +13,7 @@ import com.nelioalves.cursomc.security.util.JWTUtil;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -25,13 +27,14 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     private JWTUtil jwtUtil;
 
     public JWTAuthenticationFilter(AuthenticationManager authenticationManager, JWTUtil jwtUtil) {
+        setAuthenticationFailureHandler(new JWTAuthenticaionFailureHandler());
         this.authenticationManager = authenticationManager;
         this.jwtUtil = jwtUtil;
     }
 
     /**
      * Tenta fazer a autenticação de usuário
-     * @param httpServletRequest  Requisição feita pelo usuário
+     * @param httpServletRequest Requisição feita pelo usuário
      * @param httpServletResponse Resposta a ser enviada pelo usuário
      * @return Um Authentication
      * @throws AuthenticationException
@@ -57,11 +60,10 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
     /**
      * Retorna um token JWT para o usuário caso a autenticação tenha ocorrido com sucesso
-     *
-     * @param httpServletRequest  Requisição feita pelo usuário
+     * @param httpServletRequest Requisição feita pelo usuário
      * @param httpServletResponse Resposta a ser enviada para o usuário
-     * @param filterChain         Objeto usado para chamada de funções posteriores na cadeia de responsabilidade
-     * @param authentication      Autenticação feita anteriormente em attemptAuthentication
+     * @param filterChain Objeto usado para chamada de funções posteriores na cadeia de responsabilidade
+     * @param authentication Autenticação feita anteriormente em attemptAuthentication
      * @throws IOException
      * @throws ServletException
      */
@@ -74,4 +76,44 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         String token = jwtUtil.generateToken(username);
         httpServletResponse.addHeader("Authorization", "Bearer " + token);
     }
+
+    /**
+     * Classe que representa um manipulador de falha para erros de login
+     * @author José Henrique
+     */
+    private class JWTAuthenticaionFailureHandler implements AuthenticationFailureHandler {
+
+        /**
+         * Método chamado para montar resposta do servidor em caso de erro de login
+         * @param httpServletRequest Requisição do usuário
+         * @param httpServletResponse Resposta a ser enviada para o usuário
+         * @param authenticationException Exceção de autenticação
+         * @throws IOException
+         * @throws ServletException
+         */
+        @Override
+        public void onAuthenticationFailure(
+                HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+                AuthenticationException authenticationException) throws IOException, ServletException {
+
+            httpServletResponse.setStatus(401);
+            httpServletResponse.setContentType("application/json");
+            httpServletResponse.getWriter().append(json());
+        }
+
+        /**
+         * Monta o JSON de resposta
+         * @return Um JSON
+         */
+        private String json() {
+            long date = new Date().getTime();
+            return "{\"timestamp\": " + date + ", " +
+                    "\"status\": 401" +
+                    "\"error\": Não autorizado" +
+                    "\"message\": \"Email ou senha inválidos\"" +
+                    "\"path\": \"/login\"" +
+                    "}";
+        }
+    }
+
 }
